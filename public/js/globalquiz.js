bb58a763f132a3b70546b31f9e66970772939e71
Unknown <bernardfuriatti@gmail.com>
2017-07-20 11:54:35 -0300

    Onload -

--- public/js/globalquiz.js
+++ public/js/globalquiz.js
@@ -6,9 +6,7 @@ $( document ).ready(function() {
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   // When the user clicks on the button, open the modal
-  btn.onload = function() {
-      modal.style.display = "block";
-  }
+   modal.style.display = "block";
   // When the user clicks on <span> (x), close the modal
   span.onclick = function closeModal() {
       modal.style.display = "none";
