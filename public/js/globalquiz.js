f4e230855246ad275c92c47ecf1514e772575f88
Unknown <bernardfuriatti@gmail.com>
2017-07-20 11:24:22 -0300

    teste2 - quiz

--- public/js/globalquiz.js
+++ public/js/globalquiz.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   // When the user clicks on the button, open the modal
-  btn.ready = function() {
+  btn.onload = function() {
       modal.style.display = "block";
   }
   // When the user clicks on <span> (x), close the modal
