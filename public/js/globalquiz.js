016a3c29bd19bd62704aca56f39d174eed6e2229
Unknown <bernardfuriatti@gmail.com>
2017-07-20 12:55:46 -0300

    Implementação do IP

--- public/js/globalquiz.js
+++ public/js/globalquiz.js
@@ -46,7 +46,7 @@ $( document ).ready(function() {
           // The signed-in user info.
           var user = result.user;
 
-          //var userIp = response ? response.data.ip : "NOIP";
+          var userIp = response ? response.data.ip : "NOIP";
 
           firebase.database().ref('leads/').push({
             nome: user.providerData[0].displayName,
@@ -54,18 +54,17 @@ $( document ).ready(function() {
             foto: user.providerData[0].photoURL,
             provedor: user.providerData[0].providerId,
             data: Date(),
-            //ip: userIp
+            ip: userIp
           });
 
          window.location="quizModalFechado.html";
-         //modal.style.display = "none";
 
         })
-        /*.catch(function (error) {
+        .catch(function (error) {
           console.log(error);
           $('#ErrorMessage').text("Erro ao obter IP");
         });
-        */
+
     }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -98,8 +97,8 @@ $( document ).ready(function() {
             ip: userIp
           });
 
-          // window.location="video.html";
-           modal.style.display = "none";
+          window.location="quizModalFechado.html";
+
         })
         .catch(function (error) {
           $('#ErrorMessage').text("Erro ao obter IP");
