8b712a5f7da8d9db0299078c7edc52d4382a806c
Unknown <bernardfuriatti@gmail.com>
2017-07-20 11:57:04 -0300

    Quiz - Saida do onload

--- public/js/globalquiz.js
+++ public/js/globalquiz.js
@@ -6,11 +6,15 @@ $( document ).ready(function() {
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   // When the user clicks on the button, open the modal
-   modal.style.display = "block";
+  document.onload = function() {
+      modal.style.display = "block";
+  }
+  /*
   // When the user clicks on <span> (x), close the modal
   span.onclick = function closeModal() {
       modal.style.display = "none";
   }
+  */
 /*
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
